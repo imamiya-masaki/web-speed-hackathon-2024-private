fc358ef508a226c9e402cc392c8a15411a08194c
imamiya-masaki <imamiyagit@gmail.com>
2024-05-05 05:10:13 +0900

    linkコンポーネントをnext/linkにする

--- workspaces/next-app-router/src/_components/src/foundation/components/Link.tsx
+++ workspaces/next-app-router/src/_components/src/foundation/components/Link.tsx
@@ -1,13 +1,19 @@
+import NextLink from 'next/link'
+import type { UrlObject } from 'node:url';
+import type { CSSProperties } from 'react';
+type Url = string | UrlObject;
 type Props = {
   children: React.ReactNode;
-  to?: string;
-} & JSX.IntrinsicElements['a'];
+  to?: Url;
+  className?: string;
+  style?: CSSProperties;
+}; 
 
 export const Link: React.FC<Props> = ({ children, to, ...rest }) => {
   return (
     
-    <a href={to} {...rest}>
+    <NextLink href={to ?? "/"} {...rest}>
       {children}
-    </a>
+    </NextLink>
   );
 };
